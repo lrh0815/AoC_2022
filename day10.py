a51a012e4f16393af44b58c56719600ce80ed060
Lars <lars.hemmann@gmail.com>
2022-12-09 07:35:02 +0100

    Prepare day 10

--- day10.py
+++ day10.py
@@ -1,9 +1,9 @@
 from aoc_helper.AoCHelper import PuzzleSolver, AoCHelper
 
 
-class DaySolver(PuzzleSolver):
+class Day10Solver(PuzzleSolver):
     def __init__(self):
-        PuzzleSolver.__init__(self, 2022, x, None, None, True)
+        PuzzleSolver.__init__(self, 2022, 10, None, None, True)
 
     def solve_a(self, input: list[str]):
         return None
@@ -17,7 +17,7 @@ example_input1 = """"""
 example_input2 = """"""
 
 if __name__ == "__main__":
-    AoCHelper(DaySolver())\
+    AoCHelper(Day10Solver())\
         .test_with('a', example_input1.splitlines(), None)\
         .test_with('b', example_input2.splitlines(), None)\
         .solve().submit()
