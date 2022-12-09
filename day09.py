9d4a9461bd1ee6e1372c68b82ba27295c1f9d424
Lars <lars.hemmann@gmail.com>
2022-12-09 06:50:01 +0100

    Small clean up

--- day09.py
+++ day09.py
@@ -6,8 +6,7 @@ class Day9Solver(PuzzleSolver):
         PuzzleSolver.__init__(self, 2022, 9, None, None, True)
 
     def solve_a(self, input: list[str]):
-        answer = self.simulate_rope(input, 2)
-        return answer
+        return self.simulate_rope(input, 2)
 
     def simulate_rope(self, input, num_knots):
         self.x = [0 for i in range(0, num_knots)]
@@ -86,11 +85,10 @@ class Day9Solver(PuzzleSolver):
                 self.x[t] -= 1
 
     def solve_b(self, input: list[str]):
-        answer = self.simulate_rope(input, 10)
-        return answer
+        return self.simulate_rope(input, 10)
 
 
-example_input = """R 4
+example_input1 = """R 4
 U 4
 L 3
 D 1
@@ -99,7 +97,7 @@ D 1
 L 5
 R 2"""
 
-ex_imput2 = """R 5
+example_input2 = """R 5
 U 8
 L 8
 D 3
@@ -110,6 +108,6 @@ U 20"""
 
 if __name__ == "__main__":
     AoCHelper(Day9Solver())\
-        .test_with('a', example_input.splitlines(), 13)\
-        .test_with('b', ex_imput2.splitlines(), 36)\
+        .test_with('a', example_input1.splitlines(), 13)\
+        .test_with('b', example_input2.splitlines(), 36)\
         .solve().submit()
