e87c27305599a021640fafada6356def8e76f6a1
Lars <lars.hemmann@gmail.com>
2022-12-10 08:38:13 +0100

    Refactor day 10 and use advent_of_code_ocr

--- day10.py
+++ day10.py
@@ -1,4 +1,5 @@
 from aoc_helper.AoCHelper import PuzzleSolver, AoCHelper
+from advent_of_code_ocr import convert_6
 
 
 class DaySolver(PuzzleSolver):
@@ -6,49 +7,58 @@ class DaySolver(PuzzleSolver):
         PuzzleSolver.__init__(self, 2022, 10, None, None, True)
 
     def solve_a(self, input: list[str]):
+        self.sum_signal_strength = 0
         self.execute_program(input, self.add_signal_strength)
         return self.sum_signal_strength
 
     def execute_program(self, input, operation_per_cycle):
-        self.X = 1
+        self.reg_x = 1
         self.cycle = 0
-        self.sum_signal_strength = 0
-        for line in [x.split() for x in input]:
-            if line[0] == "noop":
-                self.cycle += 1
-                operation_per_cycle()
-            elif line[0] == "addx":
-                self.cycle += 1
-                operation_per_cycle()
-                self.cycle += 1
-                operation_per_cycle()
-                self.X += int(line[1])
+        for line in input:
+            match line.split():
+                case ["noop"]:
+                    self.cycle += 1
+                    operation_per_cycle()
+                case ["addx", value]:
+                    self.cycle += 1
+                    operation_per_cycle()
+                    self.cycle += 1
+                    operation_per_cycle()
+                    self.reg_x += int(value)
 
     def add_signal_strength(self):
         if self.cycle in [20, 60, 100, 140, 180, 220]:
-            self.sum_signal_strength += self.cycle * self.X
+            self.sum_signal_strength += self.cycle * self.reg_x
 
     def solve_b(self, input: list[str]):
-        self.output = [[]]
-        self.output_line = 0
+        self.output = [""]
+        self.max_line_length = 40
         self.execute_program(input, self.draw_pixel)
-        print()
-        for line in self.output:
-            print("".join(line))
-        return "dummy"
+        self.print_output()
+        return self.try_decode_output()
 
     def draw_pixel(self):
-        line_len = len(self.output[self.output_line])
-        if line_len == 40:
-            self.output.append([])
-            self.output_line += 1
-        pos = self.cycle % 40
-        delta = pos - self.X
+        if len(self.output[-1]) == self.max_line_length:
+            self.output.append("")
+        self.output[-1] += self.get_pixel()
+
+    def get_pixel(self):
+        current_horizontal_position = self.cycle % self.max_line_length
+        delta = current_horizontal_position - self.reg_x
         if delta <= 2 and delta >= 0:
-            pixel = "#"
-        else:
-            pixel = "."
-        self.output[self.output_line].append(pixel)
+            return "#"
+        return "."
+
+    def try_decode_output(self):
+        try:
+            return convert_6("\n".join(self.output))
+        except:
+            return "dummy"
+
+    def print_output(self):
+        print()
+        for line in self.output:
+            print(line)
 
 
 example_input1 = """addx 15
@@ -204,4 +214,4 @@ if __name__ == "__main__":
     AoCHelper(DaySolver())\
         .test_with('a', example_input1.splitlines(), 13140)\
         .test_with('b', example_input1.splitlines(), "dummy")\
-        .solve().submit('a')
+        .solve().submit()
