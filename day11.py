63d86d4c95a60add2ef0372868588a356275d0ce
Lars <lars.hemmann@gmail.com>
2022-12-11 09:56:39 +0100

    Refactor day 11

--- day11.py
+++ day11.py
@@ -8,35 +8,31 @@ def chunk_input(input, size):
 
 
 class Monkey(object):
-    def __init__(self, items: list[int], operation: list[str], test_value: int, true_target: int, false_target: int):
-        self.items = items
-        self.operation = self.__convert_operation(operation)
-        self.test_value = test_value
-        self.true_target = true_target
-        self.false_target = false_target
+    def __init__(self, monkey_spec: list[str]):
+        self.items = [int(x) for x in monkey_spec[1].split(":")[1].split(", ")]
+        self.operation = Monkey.__convert_operation(monkey_spec[2].split("=")[1].split())
+        self.test_value = int(monkey_spec[3].split()[3])
+        self.true_target = int(monkey_spec[4].split()[5])
+        self.false_target = int(monkey_spec[5].split()[5])
         self.num_inspections = 0
 
-    def __convert_operation(self, op: list[str]):
-        if op[2] != "old":
-            self.second_operand_value = int(op[2])
-        else:
-            self.second_operand_value = None
+    def __convert_operation(op: list[str]):
         if op[1] == "+":
-            return self.__add
+            operation = Monkey.__add
         else:
-            return self.__multiply
+            operation = Monkey.__multiply
 
-    def __add(self, old: int):
-        return old + (self.second_operand_value or old)
+        if op[2] != "old":
+            second_operand_value = int(op[2])
+            return lambda x: operation(x, second_operand_value)
+        else:
+            return lambda x: operation(x, x)
 
-    def __multiply(self, old: int):
-        return old * (self.second_operand_value or old)
+    def __add(x, y):
+        return x + y
 
-    def __str__(self):
-        return f"{self.items}, {self.operation}, {self.test_value}, {self.true_target}, {self.false_target}, {self.num_inspections}"
-
-    def receive(self, item):
-        self.items.append(item)
+    def __multiply(x, y):
+        return x * y
 
     def inspect_and_throw(self, targets: list["Monkey"], worry_divider: int, common_factor: int):
         for item in self.items:
@@ -53,9 +49,12 @@ class Monkey(object):
 
     def __throw(self, item: int, targets: list["Monkey"]):
         if item % self.test_value == 0:
-            targets[self.true_target].receive(item)
+            targets[self.true_target].__catch(item)
         else:
-            targets[self.false_target].receive(item)
+            targets[self.false_target].__catch(item)
+
+    def __catch(self, item):
+        self.items.append(item)
 
 
 class DaySolver(PuzzleSolver):
@@ -69,9 +68,7 @@ class DaySolver(PuzzleSolver):
         return self.__solve(input, 10000, 1)
 
     def __solve(self, input, rounds: int, worry_divider: int):
-        self.monkeys: list[Monkey] = []
-        for monkey_input in chunk_input(input, 7):
-            self.__parse_monkey(monkey_input)
+        self.monkeys = [Monkey(monkey_spec) for monkey_spec in chunk_input(input, 7)]
 
         common_factor = 1
         for monkey in self.monkeys:
@@ -80,20 +77,12 @@ class DaySolver(PuzzleSolver):
         for _ in range(rounds):
             for monkey in self.monkeys:
                 monkey.inspect_and_throw(self.monkeys, worry_divider, common_factor)
+
         inspections = [x.num_inspections for x in self.monkeys]
         inspections.sort()
 
         answer = inspections[-1] * inspections[-2]
         return answer
-
-    def __parse_monkey(self, input: list[str]):
-        items = [int(x) for x in input[1].split(":")[1].split(", ")]
-        operation = input[2].split("=")[1].split()
-        test_value = int(input[3].split()[3])
-        true_target = int(input[4].split()[5])
-        false_target = int(input[5].split()[5])
-        monkey = Monkey(items, operation, test_value, true_target, false_target)
-        self.monkeys.append(monkey)
 
 
 example_input1 = """"""
