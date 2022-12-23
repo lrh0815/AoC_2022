c98c9c26ebd23ef835dd556c26ae0b5ee8684477
Lars <lars.hemmann@gmail.com>
2022-12-23 22:16:17 +0100

    Solution day 22, part 2

--- day22.py
+++ day22.py
@@ -2,9 +2,6 @@ from aoc_helper.AoCHelper import *
 import re
 from dataclasses import dataclass
 from icecream import ic
-from collections import deque
-from rich import print
-from rich.progress import Progress
 
 
 @dataclass
@@ -27,34 +24,38 @@ class DaySolver(PuzzleSolver):
 
     def solve_a(self, input: list[str]):
         map, path = self.__read_input(input)
-        pos, direction = self.__folow_path(map, path, self.__traverse_edge_part1)
-        answer = 1000 * pos.y + 4 * pos.x + directions.index(direction)
+        pos, direction = self.__follow_path(map, path, self.__move_part_1)
+        answer = 1000 * (pos.y + 1) + 4 * (pos.x + 1) + directions.index(direction)
         return answer
 
-    def __folow_path(self, map: Grid, path: list, traverse_edge_func):
-        pos = Point(1, 1)
+    def __follow_path(self, map: Grid, path: list, move_func, print_map = True):
         direction = directions[0]
 
-        while not map.is_set(pos):
-            pos += (1, 0)
+        pos = self.__find_start(map)
 
         map.set(pos, direction.symbol)
         for entry in path:
             steps = int(entry[0])
             for _ in range(steps):
-                traversed_pos, traversed_direction, blocked = traverse_edge_func(map, pos, direction)
+                new_pos, new_direction, blocked = move_func(map, pos, direction)
                 if blocked:
                     break
-                direction = traversed_direction
-                pos = traversed_pos
+                direction = new_direction
+                pos = new_pos
                 map.set(pos, direction.symbol)
             direction = self.__get_next_direction(direction, entry[1])
             map.set(pos, direction.symbol)
 
-        map.print_grid(reverse_y=True)
+        if print_map: map.print_grid(reverse_y=True)
         return pos, direction
 
-    def __traverse_edge_part1(self, map: Grid, pos: Point, direction: Direction):
+    def __find_start(self, map):
+        pos = Point(0, 0)
+        while not map.is_set(pos):
+            pos += (1, 0)
+        return pos
+
+    def __move_part_1(self, map: Grid, pos: Point, direction: Direction):
         new_pos = pos + direction.increment
         if not map.is_set(new_pos):
             match direction.symbol:
@@ -87,13 +88,10 @@ class DaySolver(PuzzleSolver):
 
     def __read_input(self, input: list[str]):
         map = Grid(" ")
-        for y, line in enumerate(input):
-            if line.strip() == "":
-                break
-
+        for y, line in enumerate(input[:-2]):
             for x, tile in enumerate(line):
                 if tile != " ":
-                    map.set(Point(x + 1, y + 1), tile)
+                    map.set(Point(x, y), tile)
 
         path = re.findall(r"(\d\d?)([RL])?", input[-1])
         return map, path
@@ -106,72 +104,87 @@ class DaySolver(PuzzleSolver):
 
         map, path = self.__read_input(input)
         map.print_grid(reverse_y=True)
-        pos, direction = self.__folow_path(map, path, self.__traverse_edge_part2)
-        answer = 1000 * ic(pos.y) + 4 * ic(pos.x) + ic(directions.index(direction))
+
+        self.__test_movement(map)
+
+        pos, direction = self.__follow_path(map, path, self.__move_part_2)
+        answer = 1000 * ic(pos.y + 1) + 4 * ic(pos.x + 1) + ic(directions.index(direction))
         return answer
 
-    def __traverse_edge_part2(self, map: Grid, pos: Point, direction: Direction):
+    def __test_movement(self, map: Grid):
+        steps = 4 * self.cs
+        start_pos = self.__find_start(map)
+
+        map_copy = Grid(map.default_value)
+
+        for i in range(4):
+            for p in map.all_set_points():
+                map_copy.set(p, ".")
+            path = [[0, "R"]] * i + [[steps, ""]]
+            pos, direction = self.__follow_path(map_copy, path, self.__move_part_2, False)
+            assert pos == start_pos
+            assert direction == directions[i]
+
+    def __move_part_2(self, map: Grid, pos: Point, direction: Direction):
         new_pos = pos + direction.increment
         cs = self.cs
-        match new_pos:
-            case p if p.x in range(cs + 1, 2 * cs + 1) and p.y == 0:       # 1 -> 6
-                new_direction = right
-                new_pos = Point(1, (p.x - cs) + 3 * cs)
+        if not map.is_set(new_pos):
+            match pos:
+                case p if p.x in range(1 * cs, 2 * cs) and p.y in range(0 * cs, 1 * cs):  # 1
+                    if direction == up: # -> 6
+                        new_direction = right
+                        new_pos = Point(0, (p.x - cs) + 3 * cs)
+                    if direction == left: # -> 5
+                        new_direction = right
+                        new_pos = Point(0, (3 * cs - 1) - p.y)
 
-            case p if p.x == cs and p.y in range(1, cs + 1):        # 1 -> 5
-                new_direction = right
-                new_pos = Point(1, (3 * cs + 1) - p.y)
+                case p if p.x in range(2 * cs, 3 * cs) and p.y in range(0 * cs, 1 * cs):  # 2
+                    if direction == right: # -> 4
+                        new_direction = left
+                        new_pos = Point(2 * cs - 1, (3 * cs - 1) - p.y)
+                    if direction == up: # -> 6
+                        new_direction = up
+                        new_pos = Point((p.x - 2 * cs), 4 * cs - 1)
+                    if direction == down: # -> 3
+                        new_direction = left
+                        new_pos = Point(2 * cs - 1, (p.x - 2 * cs) + cs)
 
-            case p if p.x == 3 * cs + 1 and p.y in range(1, cs + 1):       # 2 -> 4
-                new_direction = left
-                new_pos = Point(2 * cs, (3 * cs + 1) - p.y)
+                case p if p.x in range(1 * cs, 2 * cs) and p.y in range(1 * cs, 2 * cs):  # 3
+                    if direction == left: # -> 5
+                        new_direction = down
+                        new_pos = Point((p.y - cs), 2 * cs)
+                    if direction == right: # -> 2
+                        new_direction = up
+                        new_pos = Point((p.y - cs) + 2 * cs, cs - 1)
 
-            case p if p.x in range(2 * cs + 1, 3 * cs + 1) and p.y == 0:      # 2 -> 6
-                new_direction = up
-                new_pos = Point((p.x - 2 * cs), 4 * cs)
+                case p if p.x in range(1 * cs, 2 * cs) and p.y in range(2 * cs, 3 * cs):  # 4
+                    if direction == right: # -> 2
+                        new_direction = left
+                        new_pos = Point(3 * cs - 1, cs - 1 - (p.y - 2 * cs))
+                    if direction == down: # -> 6
+                        new_direction = left
+                        new_pos = Point(cs - 1, (p.x - cs) + 3 * cs)
+                
+                case p if p.x in range(0 * cs, 1 * cs) and p.y in range(2 * cs, 3 * cs):  # 5
+                    if direction == up: # -> 3
+                        new_direction = right
+                        new_pos = Point(cs, p.x + cs)
+                    if direction == left: # -> 1
+                        new_direction = right
+                        new_pos = Point(cs, cs - 1 - (p.y - 2 * cs))
 
-            case p if p.x in range(2 * cs + 1, 3 * cs + 1) and p.y == cs + 1:     # 2 -> 3
-                new_direction = left
-                new_pos = Point(2 * cs, (p.x - 2 * cs) + cs)
-
-            case p if p.x == cs and p.y in range(cs + 1, 2 * cs + 1):      # 3 -> 5
-                new_direction = down
-                new_pos = Point((p.y - cs), 2 * cs + 1)
-
-            case p if p.x == 2 * cs + 1 and p.y in range(cs + 1, 2 * cs + 1):     # 3 -> 2
-                new_direction = up
-                new_pos = Point((p.y - cs) + 2 * cs, cs)
-
-            case p if p.x == 2 * cs + 1 and p.y in range(2 * cs + 1, 3 * cs + 1):    # 4 -> 2
-                new_direction = left
-                new_pos = Point(3 * cs, (cs + 1) - (p.y - 2 * cs))
-
-            case p if p.x in range(cs + 1, 2 * cs + 1) and p.y == 3 * cs + 1:     # 4 -> 6
-                new_direction = left
-                new_pos = Point(cs, (p.x - cs) + 3 * cs)
-
-            case p if p.x in range(1, cs + 1) and p.y == 2 * cs:       # 5 -> 3
-                new_direction = right
-                new_pos = Point(cs + 1, p.x + cs)
-
-            case p if p.x == 0 and p.y in range(2 * cs + 1, 3 * cs + 1):    # 5 -> 1
-                new_direction = right
-                new_pos = Point(cs + 1, (cs + 1) - (p.y - 2 * cs))
-
-            case p if p.x == cs + 1 and p.y in range(3 * cs + 1, 4 * cs + 1):     # 6 -> 4
-                new_direction = up
-                new_pos = Point((p.y - 3 * cs) + cs, 3 * cs)
-
-            case p if p.x == 0 and p.y in range(3 * cs + 1, 4 * cs + 1):      # 6 -> 1
-                new_direction = down
-                new_pos = Point((p.y - 3 * cs) + cs, 1)
-
-            case p if p.x in range(1, cs + 1) and p.y == 4 * cs + 1:       # 6 -> 2
-                new_direction = down
-                new_pos = Point(p.x + 2 * cs, 1)
-
-            case _:
-                new_direction = direction
+                case p if p.x in range(0 * cs, 1 * cs) and p.y in range(3 * cs, 4 * cs):  # 6
+                    if direction == right: # -> 4
+                        new_direction = up
+                        new_pos = Point((p.y - 3 * cs) + cs, 3 * cs - 1)
+                    if direction == left: # -> 1
+                        new_direction = down
+                        new_pos = Point((p.y - 3 * cs) + cs, 0)
+                    if direction == down: # -> 2
+                        new_direction = down
+                        new_pos = Point(p.x + 2 * cs, 0)
+        else:
+            new_direction = direction
 
         if direction != new_direction:
             ic(pos, direction, new_pos, new_direction)
@@ -207,4 +220,6 @@ if __name__ == "__main__":
     AoCHelper(DaySolver())\
         .test("a")\
         .test_with('b', example_input2.splitlines(), 10 * 1000 + 1 * 4 + 2)\
-        .solve("b").submit()
+        .solve().submit()
+
+
